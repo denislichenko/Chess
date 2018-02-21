1b4751243934e0261e90ceb90a8fd46de7d20891
Denis <vip.lichenko@gmail.com>
2018-02-21 19:51:00 +0200

    Добавьте файлы проекта.

---
+++ Chess/Chess.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+    public class Chess
+    {
+        public string fem { get; private set; };
+        public Chess(string fem = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1") {
+            this.fem = fem;
+        }
+
+        public Chess Move(string mov)
+        {
+            Chess nextChess = new Chess(fen);
+            return nextChess; 
+        }
+
+        public char GetFigureAt(int x, int y)
+        {
+            return '.';
+        }
+    } 
+}
---
+++ DemoChess/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chess; 
+
+namespace DemoChess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Chess.Chess chess = new Chess.Chess();
+
+            while (true) {
+                Console.WriteLine(chess.fem);
+                string move = Console.ReadLine();
+                if (move == "") break;
+                chess = chess.Move(move);
+            }
+        }
+    }
+}
